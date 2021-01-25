6b9f49320f386e00636a639f11bef82249ace017
zhuzhiting <804922525@qq.com>
2021-01-25 15:23:45 +0800

    前端表单验证完成

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,25 +1,57 @@
 import React, {Component} from 'react';
-import {Input, Button,Form,Checkbox} from "antd"
+import {
+    Form,
+    Input,
+    Button,
+} from 'antd'
 import { UserOutlined,LockOutlined } from '@ant-design/icons';
 import './Login.less';
 
 class Login extends Component {
+
+    handleSubmit = (e) => {
+        console.log(1)
+        // 阻止事件默认行为(不提交表单)
+        e.preventDefault()
+        // 进行表单所有控件的校验
+        this.props.form.validateFields(async (err, values) => {
+            if (!err) {
+                // 校验成功
+                const {username, password} = values
+                console.log('提交登陆请求', username, password)
+            } else {
+                // 校验失败
+                console.log(err)
+            }
+        })
+    }
+
     render() {
+
        return (
            <div className="login">
                <header className="login-header">
-                   <h1>React 后台管理系统</h1>
+                   <h1>React后台管理系统</h1>
                </header>
                <section className="login-content">
                    <h2>用户登陆</h2>
-                   <Form className="login-form" >
-                       <Form.Item>
+                   <Form className="login-form" onFinish={this.handleSubmit.bind(this)}
+                         initialValues={{"username":"admin","password":"admin"}}>
+                       <Form.Item rules={[
+                           {required: true, message: '用户名不能为空'},
+                           {min: 4, message: '用户名至少4位'},
+                           {max: 12, message: '用户名最多12位'},
+                           {pattern: /^[A-z0-9_]+$/, message: '用户名必须是英文、数字或者下划线组成'},
+                           {whitespace: true}]} name='username'>
                            <Input  prefix={<UserOutlined />} placeholder="用户名"/>
                        </Form.Item>
 
-                       <Form.Item>
-                           <Input.Password
-                               prefix={<LockOutlined />} placeholder="密码"/>
+                       <Form.Item  rules={[
+                           {required: true, message: '密码不能为空'},
+                           {min: 4, message: '用户名至少4位'},
+                           {max: 12, message: '用户名最多12位'},
+                           {whitespace: true}]} name='password'>
+                           <Input.Password prefix={<LockOutlined />} placeholder="密码"/>
                        </Form.Item>
 
                        <Form.Item>
