bedefd23ac5adf5624bb630d2783204428e8ec03
zrjGit-only <1401291913@qq.com>
2021-01-25 09:21:52 +0800

    D:/Git/Git/login /admin路由环境搭建完成

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
-import React, {Component} from 'react';
+import React, {Component, lazy, Suspense} from 'react';
+import {NavLink, Switch, BrowserRouter, Route, Redirect,} from "react-router-dom"
+
+const Login = lazy(() => import(/*webpackChunkName:"three"*/"../pages/Login/Login"));
+const Admin = lazy(() => import(/*webpackChunkName:"child"*/"../pages/Admin/Admin"));
 
 class App extends Component {
     render() {
         return (
-            <div>
-                APP
-            </div>
+            <>
+                <NavLink to='/login'></NavLink>
+                <NavLink to='/admin'></NavLink>
+
+                <Switch>
+                    <Suspense fallback="<h1>加载中.....</h1>">
+                        <Route path='/admin' component={Admin}/>
+                        <Route path='/' component={Login}/>
+                        <Redirect to="/login"></Redirect>
+                    </Suspense>
+                </Switch>
+            </>
         )
     }
 }
