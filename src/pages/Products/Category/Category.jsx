4687495966d7dc4713bca9633dadcd679fd07994
zhuzhiting <804922525@qq.com>
2021-01-29 18:36:21 +0800

    category静态页面

--- src/pages/Products/Category/Category.jsx
+++ src/pages/Products/Category/Category.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {
+    Card,
+    Table,
+    Button,
+    message,
+    Modal
+} from 'antd'
+import { PlusOutlined } from '@ant-design/icons';
 
+import LinkButton from "../../../components/LinkButton/LinkButton"
 export default function Category() {
+    const title="一级分类列表"
+    const extra=(
+        <Button type='primary'>
+            <PlusOutlined/>
+            添加
+        </Button>
+    )
+    const dataSource = [
+        {
+            key: '1',
+            name: '胡彦斌',
+            age: 32,
+            address: '西湖区湖底公园1号',
+        },
+        {
+            key: '2',
+            name: '胡彦祖',
+            age: 42,
+            address: '西湖区湖底公园1号',
+        },
+    ];
+
+    const columns = [
+        {
+            title: '分类名称',
+            dataIndex: 'name',
+        },
+        {
+            title: '操作',
+            width:300,
+            render:()=>(//返回需要显示界面的标签
+                <span>
+                    <LinkButton>修改分类</LinkButton>
+                    <LinkButton>查看子分类</LinkButton>
+                </span>
+            )
+        },
+
+    ];
     return (
-        <div>
-            Category
-        </div>
+        <Card title={title} extra={extra}>
+            <Table
+                rowkey="_id"
+                dataSource={dataSource}
+                columns={columns}
+                bordered={true}></Table>
+        </Card>
     )
 }
