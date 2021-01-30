03f557479283e5996d32dc24f157d34362ea63c0
zhuzhiting <804922525@qq.com>
2021-01-30 15:07:28 +0800

    category组件-异步显示一级分类列表

--- src/api/http.js
+++ src/api/http.js
@@ -2,3 +2,6 @@ import axiosApi from './axiosApi'
 //获取角色列表
 export const getRoleInfo = () => axiosApi.get('/manage/role/list')
 export const updateRoleInfo = (RoleInfo) => axiosApi.post('/manage/role/update',RoleInfo)
+
+// 获取一级/二级分类的列表
+export const reqCategorys = (parentId) => axiosApi.get('/manage/category/list', parentId)
--- src/pages/Products/Category/Category.jsx
+++ src/pages/Products/Category/Category.jsx
@@ -4,58 +4,90 @@ import {
     Table,
     Button,
     message,
-    Modal
+    Modal,
 } from 'antd'
 import { PlusOutlined } from '@ant-design/icons';
 
 import LinkButton from "../../../components/LinkButton/LinkButton"
-export default function Category() {
-    const title="一级分类列表"
-    const extra=(
-        <Button type='primary'>
-            <PlusOutlined/>
-            添加
-        </Button>
-    )
-    const dataSource = [
-        {
-            key: '1',
-            name: '胡彦斌',
-            age: 32,
-            address: '西湖区湖底公园1号',
-        },
-        {
-            key: '2',
-            name: '胡彦祖',
-            age: 42,
-            address: '西湖区湖底公园1号',
-        },
-    ];
+import {reqCategorys} from "../../../api/http"
+export default class Category extends Component {
+    state={
+        categorys:[]//一级分类列表
+    }
 
-    const columns = [
-        {
-            title: '分类名称',
-            dataIndex: 'name',
-        },
-        {
-            title: '操作',
-            width:300,
-            render:()=>(//返回需要显示界面的标签
-                <span>
-                    <LinkButton>修改分类</LinkButton>
-                    <LinkButton>查看子分类</LinkButton>
-                </span>
-            )
-        },
 
-    ];
-    return (
-        <Card title={title} extra={extra}>
-            <Table
-                rowkey="_id"
-                dataSource={dataSource}
-                columns={columns}
-                bordered={true}></Table>
-        </Card>
-    )
+    /*初始化Table所有列的数组*/
+    initColumns = () => {
+        this.columns = [
+            {
+                title: '分类的名称',
+                dataIndex: 'name', // 显示数据对应的属性名
+            },
+            {
+                title: '操作',
+                width: 300,
+                render: (category) => ( // 返回需要显示的界面标签
+                    <span>
+                        <LinkButton >修改分类</LinkButton>
+                        <LinkButton >查看子分类</LinkButton>
+                    </span>
+                )
+            }
+        ]
+    }
+
+    /* 异步获取一级分类列表*/
+    getCategorys= async()=>{
+        //发送请求获取数据
+        const result=await reqCategorys("0")
+        if (result.status===0){
+            const categorys = result.data
+            this.setState({categorys})//更新状态
+        }else{
+            message.error("获取分类列表失败")
+        }
+    }
+
+    /*为第一次render准备数据*/
+    componentDidMount() {
+        this.initColumns()
+    }
+
+    render(){
+        const title="一级分类列表"
+        const extra=(
+            <Button type='primary'>
+                <PlusOutlined/>
+                添加
+            </Button>
+        )
+
+
+        const dataSource = [
+            {
+                key: '1',
+                name: '胡彦斌',
+                age: 32,
+                address: '西湖区湖底公园1号',
+            },
+            {
+                key: '2',
+                name: '胡彦祖',
+                age: 42,
+                address: '西湖区湖底公园1号',
+            },
+        ];
+
+
+        return (
+            <Card title={title} extra={extra}>
+                <Table
+                    rowkey="_id"
+                    dataSource={dataSource}
+                    columns={this.columns}
+                    bordered={true}></Table>
+            </Card>
+        )
+    }
+
 }
