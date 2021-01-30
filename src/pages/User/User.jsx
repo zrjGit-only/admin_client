794ceee381fb75494fce0fdffb9e107ea49febc2
zrjGit-only <1401291913@qq.com>
2021-01-30 14:57:23 +0800

    用户管理创建与修改静态页面完成

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,27 +1,40 @@
-import React, {useEffect,useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {connect} from 'react-redux'
 import userAction from '../../store/actions/user'
 import {delUserInfo} from '../../api/http'
-import {Button, Card, Popconfirm, Input, Modal, Table, Space,message} from "antd";
+import {Button, Card, Popconfirm, Input, Modal, Table, Space, message} from "antd";
+import AddOrUpDataUser from './addOrUpdataUser/addOrUpdataUser'
 
 const {Column} = Table;
 
 function User(props) {
-    const [isRefresh,setIsRefresh] = useState(false)
+    const [isAddOrUpData, setIsAddOrUpData] = useState(0)     //显示增加/修改界面
+    const [isRefresh, setIsRefresh] = useState(false)    //重新获取列表信息
     useEffect(() => {
         const getUserInfo = async () => {
             await props.getUserInfoStore()
         }
         getUserInfo()
     }, [isRefresh])
-
-    const confirm=async (text)=> {
+    //确认删除
+    const confirm = async (text) => {
         // console.log(text);
         await delUserInfo(text._id)
         message.success('删除成功');
         setIsRefresh(!isRefresh)
     }
-    const title = <Button type="primary">创建用户</Button>
+    //确认增加用户
+    const handleOk = () => {
+
+    }
+    //确认取消添加
+    const handleCancel = () => {
+        setIsAddOrUpData(0)
+    }
+
+    const title = <Button type="primary" onClick={() => {
+        setIsAddOrUpData(1)
+    }}>创建用户</Button>
     return (
         <Card title={title} style={{width: '100%'}}>
             <Table dataSource={props.userInfo}>
@@ -35,10 +48,13 @@ function User(props) {
                     key="caozuo"
                     render={(text, record) => (
                         <Space size="middle">
-                            <a>修改</a>
+                            <a onClick={() => {
+                                console.log(11);
+                                setIsAddOrUpData(2)
+                            }}>修改</a>
                             <Popconfirm
                                 title="确定删除此用户吗？"
-                                onConfirm={()=>confirm(text)}
+                                onConfirm={() => confirm(text)}
                                 okText="Yes"
                                 cancelText="No">
                                 <a href="#">删除</a>
@@ -47,6 +63,12 @@ function User(props) {
                     )}
                 />
             </Table>
+            <Modal title={isAddOrUpData === 1 ? '添加用户' : '修改用户'}
+                   visible={isAddOrUpData !==0 }
+                   onOk={handleOk}
+                   onCancel={handleCancel}>
+                <AddOrUpDataUser/>
+            </Modal>
         </Card>
 
     )
---
+++ src/pages/User/addOrUpdataUser/addOrUpdataUser.jsx
@@ -0,0 +1,71 @@
+import React, {useEffect, useState} from 'react'
+import {connect} from 'react-redux'
+import {Input, Form, Select} from "antd";
+
+const {Option} = Select;
+
+function AddOrUpDataUser(props) {
+    const onGenderChange = () => {
+    }
+    const layout = {
+        labelCol: {span:6},
+        wrapperCol: {span: 14},
+    };
+    return (
+        <Form {...layout}>
+            <Form.Item
+                label="用户名"
+                name="username"
+                rules={[{required: true, message: '请输入用户名'}]}
+            >
+                <Input placeholder="请输入用户名"/>
+            </Form.Item>
+            <Form.Item
+                label="密码"
+                name="password"
+                rules={[{required: true, message: '请输入密码'}]}>
+                <Input.Password placeholder="请输入密码"/>
+            </Form.Item>
+            <Form.Item
+                label="手机号"
+                name="phone"
+                rules={[{required: true, message: '请输入手机号'}]}>
+                <Input placeholder="请输入密码"/>
+            </Form.Item>
+            <Form.Item
+                label="邮箱"
+                name="email"
+                rules={[{required: true, message: '请输入邮箱'}]}>
+                <Input placeholder="请输入密码"/>
+            </Form.Item>
+            <Form.Item name="role_id" label="角色" rules={[{required: true}]}>
+                <Select
+                    placeholder="请选择角色"
+                    onChange={onGenderChange}
+                    allowClear
+                >
+                    <Option value="male">male</Option>
+                    <Option value="female">female</Option>
+                    <Option value="other">other</Option>
+                </Select>
+            </Form.Item>
+        </Form>
+    )
+}
+
+function mapStateToProps(state) {
+    console.log(state);
+    return {
+        userInfo: state.user.userInfo,
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        async getUserInfoStore(parentId) {
+            // await dispatch(userAction.getUserInfo(parentId))
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(AddOrUpDataUser);
