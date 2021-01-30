f7cb29b207036e3ddaba77157a39c238fc91f125
zhuzhiting <804922525@qq.com>
2021-01-31 00:13:08 +0800

    product

--- src/pages/Products/Product/home.jsx
+++ src/pages/Products/Product/home.jsx
@@ -19,7 +19,7 @@ export default class ProductHome extends Component {
 
     state = {
         total: 0, // 商品的总数量
-        products: [], // 商品的数组
+        products: [], // 商品 的数组
         loading: false, // 是否正在加载中
         searchName: '', // 搜索的关键字
         searchType: 'productName', // 根据哪个字段搜索
