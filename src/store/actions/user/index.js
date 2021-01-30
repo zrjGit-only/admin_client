92ae7eb0be7803eac97981563d019dbf87a83b09
zrjGit-only <1401291913@qq.com>
2021-01-30 11:11:22 +0800

    user store搭建

---
+++ src/store/actions/user/index.js
@@ -0,0 +1,21 @@
+import {GET_USER_INFO} from '../../actions-type/user'
+import {} from '../../../api/http'
+
+export const get_user_info = function (payload) {
+    return {
+        type: GET_USER_INFO,
+        payload
+    }
+}
+
+
+export default {
+    //获取商品分页列表
+    getUserInfo() {
+        //因为要传参所以返回需要的函数,外部函数用来接收参数
+        return async (dispatch) => {
+
+            // dispatch(get_user_info(productInfo.data))
+        }
+    }
+}
---
+++ src/store/reducers/user/index.js
@@ -0,0 +1,13 @@
+import user from '../../state/user'
+import {GET_USER_INFO} from '../../actions-type/user'
+
+export default function (prevState = user, action) {
+    prevState = JSON.parse(JSON.stringify(prevState));
+    switch (action.type) {
+        case GET_USER_INFO:
+            prevState.userInfo = action.payload;
+            break;
+    }
+    // console.log(prevState,'prevState');
+    return prevState
+}
