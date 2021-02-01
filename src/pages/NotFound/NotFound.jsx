ca10a2a1910492c8d6bb748b17e034b907da3580
zrjGit-only <1401291913@qq.com>
2021-02-01 15:10:30 +0800

    404自动跳转至首页

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {Button} from 'antd'
 import './NotFound.less'
 
 function NotFound(props) {
+    const [num, setNum] = useState(5)
+    useEffect(() => {
+        setInterval(() => {
+            setNum(preState => {
+                return preState - 1
+            })
+        }, 1000)
+    }, [])
+
+    useEffect(() => {
+        if (num === 0) {
+            props.history.replace('/home')
+        }
+    }, [num])
     const goHome = () => {
-        props.history.replace('/home')
+
     }
     return (
         <div className='not-found'>
             <img src="./404.jpg" className="img"/>
             <div>
-                <Button type='primary' onClick={goHome}> 回到首页 </Button>
+                <span>{num} &nbsp;秒后跳转至<Button type='link' onClick={goHome} size="large"> 首页 </Button></span>
+
             </div>
         </div>
     )
