dca35beeb6f3f9e00b244e8a0207d5248baea730
zrjGit-only <1401291913@qq.com>
2021-02-02 10:12:42 +0800

    图形图标-柱状图静态页面搭建

--- src/pages/Charts/Bar/Bar.jsx
+++ src/pages/Charts/Bar/Bar.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import {Card, Button} from 'antd';
+import './Bar.less'
 
 export default function Bar() {
+    const update = () => {
+
+    }
     return (
-        <div>
-            Bar
-        </div>
+        <>
+            <Card style={{width: "100%"}}>
+                <Button type='primary' onClick={update}>更新</Button>
+            </Card>
+            <Card title='柱状图一'>
+            </Card>
+        </>
+
 
     )
 }
