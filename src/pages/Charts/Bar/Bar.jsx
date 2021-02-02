70a00e76a9fb96d8536b08caf69f763b0be85984
zrjGit-only <1401291913@qq.com>
2021-02-02 10:54:29 +0800

    柱状图初始化

--- src/pages/Charts/Bar/Bar.jsx
+++ src/pages/Charts/Bar/Bar.jsx
@@ -1,20 +1,59 @@
-import React from 'react'
+import React,{useState} from 'react'
 import {Card, Button} from 'antd';
+import ReactEcharts from 'echarts-for-react'
 import './Bar.less'
 
 export default function Bar() {
+
     const update = () => {
 
     }
+    const getOption = () => {
+        return {
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+                    type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+                }
+            },
+            grid: {
+                left: '3%',
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
+            },
+            xAxis: [
+                {
+                    type: 'category',
+                    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                    axisTick: {
+                        alignWithLabel: true
+                    }
+                }
+            ],
+            yAxis: [
+                {
+                    type: 'value'
+                }
+            ],
+            series: [
+                {
+                    name: '直接访问',
+                    type: 'bar',
+                    barWidth: '60%',
+                    data: [10, 52, 200, 334, 390, 330, 220]
+                }
+            ]
+        }
+    }
     return (
         <>
-            <Card style={{width: "100%"}}>
+            <Card  style={{width: "100%"}}>
                 <Button type='primary' onClick={update}>更新</Button>
             </Card>
             <Card title='柱状图一'>
+                <ReactEcharts option={getOption()} />
             </Card>
         </>
-
-
     )
 }
